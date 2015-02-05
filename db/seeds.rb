d737d4e96ed5da2475c79aa9762c16918ee2b7d3
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-02-04 19:15:20 -0500

    Made pictures show at /pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+Picture.create!(
+	:title => "The old church",
+	:artist => "Sergey Ershov",
+	:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+	)
+Picture.create!(
+	:title => "Sea Power",
+	:artist => "Stephen Scullion",
+	:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+	)
+Picture.create!(
+	:title => "Night Texts Pt 2",
+	:artist => "Mike Headford",
+	:url => "http://michaelheadford.com/images/chi_texts.jpg"
+	)
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
